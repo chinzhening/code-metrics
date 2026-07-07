32b6084e7ac02067ed6ae6da53ea0c4e72383c54
Chin Zhe Ning <ning.chin03@gmail.com>
2026-07-07 21:02:13 +0800

    use render

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
 	}
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: http.HandlerFunc(helloWorld),
+	}
+
+	log.Println("Server listening on http://localhost:" + port)
+
 	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("Failed to listen to server", err)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal("Error starting server", err)
 	}
 }
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
+func helloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
 }
